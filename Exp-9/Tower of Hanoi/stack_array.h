1bf2d1a0275db91c113e718626b16cbb3198ff70
omprakashbabu <116138027+omprakashbabu@users.noreply.github.com>
2023-10-12 13:12:40 +0530

    Update stack_array.h

--- Exp-9/Tower of Hanoi/stack_array.h
+++ Exp-9/Tower of Hanoi/stack_array.h
@@ -1,15 +1,17 @@
-#ifndef STACK_ARRAY_H
-#define STACK_ARRAY_H
-struct Stack {
+#ifndef stack_h
+#define stack_h
+#define max 20
+struct stack{
     int top;
     unsigned capacity;
-    int* array;
+    int* arr;
 };
-struct Stack* createStack(unsigned capacity);
-int isFull(struct Stack* stack);
-int isEmpty(struct Stack* stack);
-void push(struct Stack* stack, int item);
-int pop(struct Stack* stack);
-int peek(struct Stack* stack);
-void displayStack(struct Stack* Stack);
-#endif
+struct stack* create(unsigned capacity);
+int isfull(struct stack* s);
+int isempty(struct stack* s);
+void push(struct stack* s,int item);
+int pop(struct stack* s);
+int top(struct stack* s);
+void display(struct stack* s);
+void toh(int n,struct stack* s,struct stack* a,struct stack* d);
+#endif 
